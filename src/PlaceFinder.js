56feca34557742bd75dfb7cc8195d937ca6030cb
Harikrishnan S <70973180+harikrishnanssankar@users.noreply.github.com>
2021-07-22 12:10:55 +0530

    Update PlaceFinder.js

--- src/PlaceFinder.js
+++ src/PlaceFinder.js
@@ -10,11 +10,10 @@ export default class PlaceFinder {
     long,
     country = "IN",
     limit = 7,
-    radius = 200000,
     entity = "CountrySubdivision, CountrySecondarySubdivision ,CountryTertiarySubdivision, Municipality, MunicipalitySubdivision"
   ) {
     let baseUrl = "https://api.tomtom.com/search/2/geocode/";
-    let queryString = `&limit=${limit}&lat=${lat}&lon=${long}&raduis=${radius}&countrySet=${country}&key=${this.apiKey}&idxSet=Geo&entityTypeSet=${entity}`;
+    let queryString = `&limit=${limit}&countrySet=${country}&key=${this.apiKey}&idxSet=Geo&entityTypeSet=${entity}`;
     let response = await axios.get(`${baseUrl}/${query}.json?${queryString}`);
     return response.data.results;
   }
